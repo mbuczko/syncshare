7b5045267e7ad08fa0ac462b91b995555712e357
Michal Buczko <michal.buczko@gmail.com>
2013-08-22 00:07:38 +0200

    got rid of easyXDM. added own RPC.

--- demo/public/client.js
+++ demo/public/client.js
@@ -1,45 +1,135 @@
-var Syncshare = (function() {
+var Syncshare = Syncshare || {};
 
-    var handlers = [];
+Syncshare.Client = function(host, options) {
+    return {
+        connect: function(callback) {
+			new Syncshare.Service(host, options || {}).then(callback);
+            return this;
+        }
+    };
+};
 
-    function connect(url, callback) {
-        var socket = new easyXDM.Rpc({remote: url}, {
-            local: {
-                reply: function(json, successFn, errorFn) {
-                    if (json && json.call && handlers[json.call]) {
-                        handlers[json.call](json.data, json.type === 'broadcast');
-                    }
-                }
-            },
-            remote: {
-                call: function() {}
+Syncshare.Service = function(host, options) {
+    var self = this, handlers = {}, frame, rpc, deferred = new Syncshare.Deferred();
+
+    this.host = host;
+    this.timeout = options.timeout || 60000;
+    this.token = options.token || "";
+	this.session = {
+        on: function(fn, callback) {
+            handlers[fn] = callback;
+            return this;
+        },
+        call: function(fn, params, deferred) {
+            var dfid = deferred ? deferred.id() : null;
+
+            if (dfid != null) {
+                handlers[dfid] = deferred;
             }
-        });
+            frame.contentWindow.postMessage({call: fn, data: params, deferred: dfid}, '*');
+            return this;
+        },
+        rpc: function(callback) {
+            rpc = rpc || new Syncshare.Rpc(this);
+            return rpc;
+        }
+	};
 
-        var session = {
-            on: function(fn, callback) {
-                handlers[fn] = callback;
-                return this;
-            },
-            call: function(fn, params) {
-                if (socket) {
-                    socket.call(JSON.stringify({call: fn, data: (params || {})}));
-                }
-            }
-        };
+	var session = this.session;
 
-        if (callback && socket && session) {
-            callback(session);
-        } else {
-            throw "Syncshare: could not establish connection.";
+    window.addEventListener('message', function(response) {
+        var data = response.data,
+			call = data.call,
+            dfid = data.deferred;
+
+        if (call && handlers[dfid || call]) {
+			if (dfid) {
+				if (data.success) {
+					// connection established. expose session.
+					if (call === '_connect') {
+						console.log('Connection established => remotes', data.data);
+
+						session.remotes = data.data;
+						deferred.resolve(session);
+					} else {
+						handlers[dfid].resolve(data.data);
+					}
+				} else {
+					handlers[dfid].reject(data.data);
+				}
+				delete handlers[dfid];
+			} else {
+				handlers[call](data.data, data.type === 'broadcast');
+			}
+        }
+    }, false);
+
+    frame = document.createElement('iframe');
+    frame.width = frame.height = '0';
+    frame.src = this.host + '?token='+this.token+'&timeout='+this.timeout;
+
+    document.body.appendChild(frame);
+	
+	frame.onload = function() {
+		session.call('_connect', null, deferred);
+	};
+    return deferred;
+};
+
+Syncshare.Rpc = function(service) {
+	for (var deferred,len,fn,i=0,n=service.remotes.length; i<n; i++) {        
+		fn = service.remotes[i];        
+		this[fn] = function() {
+			deferred = new Syncshare.Deferred();
+			service.call(fn, Array.prototype.slice.call(arguments), deferred);
+			return deferred;
+		};
+	};
+};
+
+Syncshare.Deferred = function() {
+    var dfid, result, callbacks = {
+        resolved : [],
+        rejected : []
+    };
+
+    function done(solution, res) {
+        var fns = callbacks[solution];
+
+        for (var i=0, n=fns.length; i<n; i++) {
+            fns[i].call(null, res);
         }
 
-        return session;
+        result = res;
+        return true;
     };
 
+	// generate unique identifier
+	dfid = new Date().getTime();
 
     return {
-        connect: connect
+        id: function() { return dfid; },
+        then: function(successFn, errorFn, context) {
+            if (this.isResolved) {
+                successFn.call(context, result);
+            } else
+            if (this.isRejected) {
+                errorFn.call(context, result);
+            } else {
+                if (successFn) {
+                    callbacks.resolved.push(successFn);                    
+                }
+                if (errorFn) {
+                    callbacks.rejected.push(errorFn);                    
+                }
+            }
+            return this;
+        },
+        resolve: function(arg) {
+            this.isResolved = done('resolved', arg);
+        },
+        reject: function(arg) {
+            this.isRejected = done('rejected', arg);
+        }
     };
-
-}());
+};
--- priv/twitter.js
+++ priv/twitter.js
@@ -1,63 +1,128 @@
 console.log('loaded twitter.js provider');
 
-
 Syncshare = (function(service, proto, auth) {
+    var channel, remotes = {};
 
-    function connect(callback) {
-        var channel, socket;
-
-        if ("WebSocket" in window && (proto === 'auto' || proto === 'websockets')) {
-
-            console.log('going with WEBSOCKETS', service);
-
-            channel = new WebSocket("ws://" + 'localhost:8080/syncshare/wbs/' + service);
-            channel.onopen = function() {
-                console.log('Connected');
-                socket = callback ? callback(this) : null;
-            };
-            channel.onmessage = function(evt) {
-                if (socket && evt.data) {
-                    var data = evt.data.split("|", 2);
-                    socket.reply({call: data[0], type: data[1], data: JSON.parse(evt.data.substring(data[0].length+data[1].length+2))});
-                }
-            };
-            channel.onclose = function() {
-                console.log('Connection closed');
-            };
-        } else {
-            console.log('going with SSE', service);
-            
-            channel = new EventSource('/syncshare/sse/' + service);
-            // channel.addEventListener('message', function(reply) { rpc.postMessage({type: 'message', payload: reply.data}); });
-            // channel.addEventListener('broadcast', function(reply) { rpc.postMessage({type: 'broadcast', payload: reply.data}); });
-        }        
-    }
-
-    function send(channel, fn, data) {
-        if (channel.send) {
-            channel.send(fn + "|" + auth + "|" + JSON.stringify(data || ""));
+    var request = function(url) {
+        var xhr, encodeQuery = function(data) {
+			var ret = [];
+			for (var d in data) { 
+				ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d])); 
+			}
+			return ret.join('&');
+		};
+        try { 
+            xhr = new XMLHttpRequest();
         }
-    }
-
-    connect(function(channel) {
-        return new easyXDM.Rpc({},{ 
-            local: { 
-                call: function(message, successFn, errorFn) {
-                    var json = JSON.parse(message);
-                    if (json.call) {
-                        send(channel, json.call, json.data);
-                    }
-                }
-            },
-            remote: {
-                reply: function() { }
-
+        catch (e) {
+            try { 
+                xhr = new ActiveXObject('Microsoft.XMLHTTP'); 
             }
-        });
-    });
+            catch (e) {
+                xhr = new ActiveXObject('Msxml2.XMLHTTP'); 
+            }
+        }
+        this.post = function(payload) {
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Method', 'POST '+url+' HTTP/1.1');
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.send('payload='+payload);
+        };
+		this.get = function(payload) {
+			if (payload) {
+				url += '?'+encodeQuery(payload);
+			}
+		    xhr.open('GET', url, true);
+			xhr.send();
+		};
+    };
+
+    var remote = function(fns) { remotes = fns; },
+		reply  = function(call, type, data, dfid, succ) { 
+			window.parent.postMessage({
+				call : call, 
+				type : type, 
+				data : data, 
+				deferred : dfid, 
+				success  : succ}, '*'); 
+		};
+
+	window.addEventListener('message', function(e) {
+		var dfid = e.data.deferred,
+			data = e.data.data, 
+			fn = e.data.call;
+
+		// remote call
+		// trying to invoke function directly
+
+		if (dfid) {
+			if (fn === '_connect') {
+				if ("WebSocket" in window && (proto === 'auto' || proto === 'websockets')) {
+
+					console.log('going with WEBSOCKETS', service);
+
+					channel = new WebSocket("ws://" + 'localhost:8080/syncshare/wbs/' + service);
+					channel.onopen = function() {
+						reply(fn, null, Object.keys(remotes), dfid, true);
+					};
+					channel.onmessage = function(e) {
+						if (e.data) {
+							var data = e.data.split("|", 2);
+							reply(data[0], data[1], JSON.parse(e.data.substring(data[0].length+data[1].length+2)));
+						}
+					};
+					channel.onclose = function() {
+						console.log('Connection closed');
+					};
+				} else {
+					console.log('going with SSE', service);
+					
+					channel = new EventSource('/syncshare/sse/' + service);
+					channel.addEventListener('message', function(response) {
+						reply(response.call, 'message', response.data);
+					});
+					channel.addEventListener('broadcast', function(response) {
+						reply(response.call, 'broadcast', response.data);
+					});
+				}
+			}
+			else
+			if (remotes[fn]) {
+				remotes[fn].apply({
+					success: function(response) { reply(fn, null, response, dfid, true);  },
+					fail:    function(response) { reply(fn, null, response, dfid, false); },
+					ajax:    request
+				}, data);
+			} else {
+				throw "No remote function "+fn;
+			}
+		} else
+
+		// regular call
+		// redirecting to opened channel
+
+		if (channel && channel.send) {
+			channel.send(fn + "|" + auth + "|" + JSON.stringify(data || ""));
+		} else {
+			new request('/syncshare/sse/' + service +'/'+ fn).post(data);
+		}            
+	}, false);
+
+    // window.parent.postMessage({rpc: Object.keys(remotes)}, '*');
 
     return {
-        connect: connect
+        remote: remote
     };
 
 })('twitter', 'auto', '123');
+
+Syncshare.remote({
+    upload: function(arg1, arg2) {
+
+        var self = this;
+
+        setTimeout(function() {
+            self.success({ok: true});
+        }, 2000);
+    }
+});
