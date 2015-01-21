e9270f867f2dd8471fbc6cdb9322d230523eab68
Michal Buczko <michal.buczko@gmail.com>
2015-01-21 22:27:11 +0100

    got rid of silly authentication

--- demo/worker.rb
+++ demo/worker.rb
@@ -8,19 +8,10 @@ Syncshare::Module.register(:host => 'localhost', :service => 'twitter') do
 
   message :upload do |msg, header|
     puts "got token -> #{msg[:token]} and data -> #{msg[:data]}"
-    reply :public, {:status => "ok true", :url => "http://onet.pl"}, header
+    reply :public, {:status => "ok true", :url => "http://linkify.me"}, header
   end
 
   message :comment do |msg, header|
-    reply [2,3], {:status => "ok true", :url => "http://onet.pl"}, header
-  end
-
-  message :authenticate do
-    puts "authenticating..."
-    {
-      :used_id => 1,
-      :name => 'Janko Muzykant',
-      :belongs => [1,2,3]
-    }
+    reply [2,3], {:status => "ok true", :url => "http://linkify.me"}, header
   end
 end
